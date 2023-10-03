24f4ba7c99ebee7cf94d913a4439d62da966e11c
Atmaram Kambli <engr.atmaram.kambli22@gmail.com>
2023-10-03 11:13:23 +0530

    add logic to calculator

--- TASK1/calculator/js/script.js
+++ TASK1/calculator/js/script.js
@@ -1,15 +1,29 @@
 let str = "";
-
 let buttons = document.querySelectorAll(".btn");
-Array.from(buttons).forEach((btn) => {
+let prev = document.getElementById("prev-input");
+
+buttons.forEach((btn) => {
     btn.addEventListener("click", () => {
-        if(btn.innerHTML == "Enter") {
-            str = eval(str);
-            document.getElementById("expr").value= str;
-            str = "";
+        if(btn.innerHTML == "Enter" || btn.innerHTML == "ans" || btn.innerHTML == "=") {
+            try {
+                prev.style.display = "block";
+                prev.innerHTML = str;
+                str = eval(str);
+                document.getElementById("expr").value= str;
+            }catch(e) {
+                document.getElementById("expr").value = "invalid expression";
+                str = "";
+            }
         }
         else if(btn.innerHTML == "clear") {
             str = "";
+            prev.style.display = "none";
+
+            document.getElementById("expr").value = str;
+        }
+        else if(btn.innerHTML == "del") {
+            let newS = str.substring(0, str.length-1);
+            str = newS;
             document.getElementById("expr").value = str;
         }
         else {
